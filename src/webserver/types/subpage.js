14a19d58a08158c8f42723533dfe9cab8557888f
jtenniswood <james@tenniswood.co.uk>
2026-04-13 16:08:31 +0100

    Enhance entity activation control in webserver files
    - Introduced a toggle for "When Entity On" functionality, allowing users to manage the activation state of entities more intuitively.
    - Added an icon picker for selecting an icon to display when the entity is active, improving visual feedback.
    - Implemented event listeners to update the sensor and icon states dynamically based on user interactions.
    
    This change enhances user experience by providing clearer controls for entity activation and visual representation.

--- src/webserver/types/subpage.js
+++ src/webserver/types/subpage.js
@@ -14,14 +14,57 @@ registerButtonType("subpage", {
         renderPreview();
       }
     ));
-    var indicatorToggle = helpers.toggleRow(
-      "Activity indicator", helpers.idPrefix + "indicator", b.sensor === "indicator"
-    );
-    indicatorToggle.input.addEventListener("change", function () {
-      b.sensor = this.checked ? "indicator" : "";
-      helpers.saveField("sensor", b.sensor);
+    var hasIconOn = b.icon_on && b.icon_on !== "Auto";
+    var whenOnEnabled = b.sensor === "indicator" || hasIconOn;
+
+    var whenOnToggle = helpers.toggleRow("When Entity On", helpers.idPrefix + "whenon-toggle", whenOnEnabled);
+    panel.appendChild(whenOnToggle.row);
+
+    var whenOnCond = condField();
+    if (whenOnEnabled) whenOnCond.classList.add("sp-visible");
+
+    var iconOnSection = document.createElement("div");
+    iconOnSection.className = "sp-field";
+    iconOnSection.appendChild(helpers.fieldLabel("Icon When On", helpers.idPrefix + "icon-on"));
+    var iconOnVal = hasIconOn ? b.icon_on : "Auto";
+    var iconOnPicker = document.createElement("div");
+    iconOnPicker.className = "sp-icon-picker";
+    iconOnPicker.id = helpers.idPrefix + "icon-on-picker";
+    iconOnPicker.innerHTML =
+      '<span class="sp-icon-picker-preview mdi mdi-' + iconSlug(iconOnVal) + '"></span>' +
+      '<input class="sp-icon-picker-input" id="' + helpers.idPrefix + 'icon-on" type="text" ' +
+      'placeholder="Search icons\u2026" value="' + escAttr(iconOnVal) + '" autocomplete="off">' +
+      '<div class="sp-icon-dropdown"></div>';
+    iconOnSection.appendChild(iconOnPicker);
+    whenOnCond.appendChild(iconOnSection);
+
+    initIconPicker(iconOnPicker, iconOnVal, function (opt) {
+      b.icon_on = opt;
+      helpers.saveField("icon_on", opt);
+      renderPreview();
     });
-    panel.appendChild(indicatorToggle.row);
+
+    panel.appendChild(whenOnCond);
+
+    whenOnToggle.input.addEventListener("change", function () {
+      if (this.checked) {
+        b.sensor = "indicator";
+        helpers.saveField("sensor", "indicator");
+        whenOnCond.classList.add("sp-visible");
+      } else {
+        b.sensor = "";
+        b.icon_on = "Auto";
+        helpers.saveField("sensor", "");
+        helpers.saveField("icon_on", "Auto");
+        whenOnCond.classList.remove("sp-visible");
+        var ionPreview = iconOnPicker.querySelector(".sp-icon-picker-preview");
+        if (ionPreview) ionPreview.className = "sp-icon-picker-preview mdi mdi-cog";
+        var ionInput = iconOnPicker.querySelector(".sp-icon-picker-input");
+        if (ionInput) ionInput.value = "Auto";
+        renderPreview();
+      }
+    });
+
     var configBtn = document.createElement("button");
     configBtn.className = "sp-action-btn";
     configBtn.style.background = "var(--accent)";
